088368bca3e752d922fd66b9858a429afe233e0a
Grzegorz Kowalski <grzegorz.adam.kowalski@outlook.com>
2016-04-12 11:18:26 +0200

    Initial commit

---
+++ ldap-proxy.js
@@ -0,0 +1,65 @@
+const assert = require('assert');
+var ldap = require('ldapjs');
+
+var server = ldap.createServer();
+
+function test_client(client, login, pass, base, scope)
+{
+  client.bind(login, pass, function (err)
+  {
+    assert.ifError(err);
+
+    var options = { scope: scope }
+
+    client.search(base, options, function (err, res) {
+      assert.ifError(err);
+
+      res.on('searchEntry', function(entry) {
+        console.log('entry: ' + JSON.stringify(entry.object));
+      });
+
+      res.on('searchReference', function(referral) {
+        console.log('referral: ' + referral.uris.join());
+      });
+
+      res.on('error', function(err) {
+        console.error('error: ' + err.message);
+      });
+
+      res.on('end', function(result) {
+        console.log('status: ' + result.status);
+        client.unbind();
+        process.exit();
+      });
+    });
+  });
+}
+
+var client_first = ldap.createClient({
+  url: 'ldap://ldap.forumsys.com'
+});
+
+var client_second = ldap.createClient({
+  url: 'ldap://www.zflexldap.com'
+});
+
+function test_prime(prime) {
+  var complex = 6;
+  return complex % prime == 0;
+}
+
+if (test_prime(2)) test_client(
+  client_first,
+  'uid=tesla,dc=example,dc=com',
+  'password',
+  'dc=example,dc=com',
+  'sub'
+);
+
+if (test_prime(3)) test_client(
+  client_second,
+  'cn=ro_admin,ou=sysadmins,dc=zflexsoftware,dc=com',
+  'zflexpass',
+  'ou=sysadmins,dc=zflexsoftware,dc=com',
+  'sub'
+);
