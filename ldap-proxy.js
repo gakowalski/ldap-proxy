c44a7b93a2f7e9a29987439b0e94cb23f9b80568
Grzegorz Kowalski <grzegorz.adam.kowalski@outlook.com>
2016-04-14 23:26:19 +0200

    Added bind support

--- ldap-proxy.js
+++ ldap-proxy.js
@@ -28,11 +28,11 @@ function ldap_search(proxy)
 {
   var args =
   [
-    '-w' + proxy.password,
-    '-H' + proxy.url,
-    '-D' + proxy.login,
-    '-b' + proxy.base,
-    '-s' + proxy.scope,
+    '-w', proxy.password,
+    '-H', proxy.url,
+    '-D', proxy.login,
+    '-b', proxy.base,
+    '-s', proxy.scope,
     '-LLL'
   ];
 
@@ -66,28 +66,85 @@ function ldap_search(proxy)
   return results;
 }
 
+function ldap_bind(url, login, password)
+{
+  var args =
+  [
+    '-w', password,
+    '-H', url,
+    '-D', login,
+    '-b', login,
+    '-s', 'base',
+    '-LLL',
+    '1.1'
+  ];
+
+  console.log('bind as ' + login);
+
+  try {
+    var output = execFileSync('ldapsearch', args, { encoding: 'utf-8'});
+  }
+  catch (err) {
+    return false;
+  }
+  return (output.substring(0,3) === 'dn:');
+}
+
 server.listen(1389, '127.0.0.1', function() {
-  console.log('LDAP server listening at ' + server.url + ' for 5 seconds');
+  console.log('LDAP server listening at ' + server.url + ' for 10 seconds');
 
   setTimeout(function () {
     console.log('LDAP server shutdown after timeout started.');
     server.close();
     console.log('LDAP server shutdown after timeout done.');
-  }, 5000);
+  }, 10000);
 });
 
-server.bind('cn=anonymous', function(req, res, next) {
+server.bind('dc=ldap,dc=grzegorzkowalski,dc=pl', function(req, res, next) {
   console.log('bind login DN: ' + req.dn.toString());
   console.log('bind password: ' + req.credentials);
+
+  if (req.credentials !== 'secret') {
+    return next(new ldap.InvalidCredentialsError());
+  }
+
   res.end();
   return next();
 });
 
-server.bind('cn=halt', function(req, res, next) {
-  server.close();
+server.bind('o=users,dc=grzegorzkowalski,dc=pl', function(req, res, next) {
+  console.log('bind login DN: ' + req.dn.toString());
+  console.log('bind password: ' + req.credentials);
+
+  var result = proxy_config.some(function (proxy) {
+    if (proxy.mount === 'o=users,dc=grzegorzkowalski,dc=pl') {
+      var result = ldap_bind(
+        proxy.url,
+        req.dn.toString().replace(ldap.parseDN('o=users,dc=grzegorzkowalski,dc=pl').toString(), proxy.base).replace(' ', ''),
+        req.credentials
+      );
+      if (result === true) {
+        return true;
+      }
+    }
+    return false
+  });
+
+  if (result === true) {
+    res.end();
+    return next();
+  }
+
+  return next(new ldap.InvalidCredentialsError());
 });
 
+
 server.search('o=users,dc=grzegorzkowalski,dc=pl', function(req, res, next) {
+
+  if (req.connection.ldap.bindDN.equals('cn=anonymous')) {
+    return next(new ldap.InsufficientAccessRightsError());
+  }
+
   proxy_config.forEach(function (proxy) {
     if (proxy.mount === 'o=users,dc=grzegorzkowalski,dc=pl') {
       var entries = ldap_search(proxy);
